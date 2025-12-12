cd3892273020eda0460d92e28ef8f5c69bfce765
jinl33 <jinl33@nycstudents.net>
2025-12-12 14:04:34 -0500

    Spring Display and Loop Logics

--- script.js
+++ script.js
@@ -18,8 +18,27 @@ springImages = ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSejJwvBNg
 summerImages = ["https://funworldfacts.com/wp-content/uploads/2024/11/facts-about-june.jpg", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIRiB3EB43cOkdBgvKuU688yoDpUuPLorIwQ&s", "https://www.madewithhappy.com/wp-content/uploads/month-of-august.jpg"];
 fallImages = ["https://media.istockphoto.com/id/1267485165/vector/september-text-lettering-typography-vector-illustration-as-poster-postcard-greeting-card.jpg?s=612x612&w=0&k=20&c=AhTukEabgsari2_JW_HbZhQpXZF4WaQnvVXOtT2qHME=", "https://media.istockphoto.com/id/1054931018/photo/hello-october-with-pumpkins-at-night.jpg?s=612x612&w=0&k=20&c=wIP93DslUKnFxmMYT_Nl9bqOoiPtKONamsMCrB65kxU=", "https://media.timeout.com/images/106193491/750/562/image.jpg"];
 
-function displayWinter() {
+let winter_container_DOM = document.querySelector(".spring");
 
+let Get_Template = (month, image_url) => {
+	let Template = `
+	<div class="month-card">
+		<img src="${image_url}" class="month-img">
+		<div class="month-body">${month}</div>
+    </div>`;
+	return Template;
+}
+
+function displayWinter() {
+	winter_container_DOM.innerHTML = "";
+	winter.forEach( (month, index) => {
+		
+		let Month_Image_URL = winterImages[index];
+
+		let Template = Get_Template(month, Month_Image_URL);
+		winter_container_DOM.insertAdjacentHTML("beforeend", Template);
+
+	});
 }
 
 
@@ -42,3 +61,5 @@ function displayAllSeasons() {
 
 	
 }
+
+displayAllSeasons();
