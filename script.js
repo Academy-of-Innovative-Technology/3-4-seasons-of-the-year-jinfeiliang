93fc262077a4e0ca2b9aac5cae211bcad1051675
jinl33 <jinl33@nycstudents.net>
2025-12-12 13:35:33 -0500

    Season months set'd

--- script.js
+++ script.js
@@ -8,7 +8,10 @@ var springImages;
 var summerImages;
 var fallImages;
 
-
+winter = ["January", "February", "December"];
+spring = ["March", "April", "May"];
+summer = ["June", "July", "May"];
+fall = ["September", "October", "November"];
 
 function displayWinter() {
 
