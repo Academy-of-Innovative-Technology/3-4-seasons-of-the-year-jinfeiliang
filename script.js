c9373d32644ccacd96edf89e829210c3ba4716f0
jinfeiliang <tinfeek80@gmail.com>
2025-12-13 22:20:13 -0500

    All seasons displaying JS

--- script.js
+++ script.js
@@ -18,7 +18,10 @@ springImages = ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSejJwvBNg
 summerImages = ["https://funworldfacts.com/wp-content/uploads/2024/11/facts-about-june.jpg", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIRiB3EB43cOkdBgvKuU688yoDpUuPLorIwQ&s", "https://www.madewithhappy.com/wp-content/uploads/month-of-august.jpg"];
 fallImages = ["https://media.istockphoto.com/id/1267485165/vector/september-text-lettering-typography-vector-illustration-as-poster-postcard-greeting-card.jpg?s=612x612&w=0&k=20&c=AhTukEabgsari2_JW_HbZhQpXZF4WaQnvVXOtT2qHME=", "https://media.istockphoto.com/id/1054931018/photo/hello-october-with-pumpkins-at-night.jpg?s=612x612&w=0&k=20&c=wIP93DslUKnFxmMYT_Nl9bqOoiPtKONamsMCrB65kxU=", "https://media.timeout.com/images/106193491/750/562/image.jpg"];
 
-let winter_container_DOM = document.querySelector(".spring");
+let spring_container_DOM = document.querySelector(".spring");
+let summer_container_DOM = document.querySelector(".summer");
+let fall_container_DOM = document.querySelector(".fall");
+let winter_container_DOM = document.querySelector(".winter");
 
 let Get_Template = (month, image_url) => {
 	let Template = `
@@ -42,36 +45,28 @@ function load_season(container_dom, months_array, images_array){
 }
 
 function displayWinter() {
-	winter_container_DOM.innerHTML = "";
-	winter.forEach( (month, index) => {
-		
-		let Month_Image_URL = winterImages[index];
-
-		let Template = Get_Template(month, Month_Image_URL);
-		winter_container_DOM.insertAdjacentHTML("beforeend", Template);
-
-	});
+	load_season(winter_container_DOM, winter, winterImages);
 }
 
 
 function displaySpring() {
-
+	load_season(spring_container_DOM, spring, springImages);
 }
 
 function displaySummer() {
-
+	load_season(summer_container_DOM, summer, summerImages);
 }
 
 function displayFall() {
-
+	load_season(fall_container_DOM, fall, fallImages);
 }
 
 
 function displayAllSeasons() {
 	displayWinter();
-
-
-	
+	displaySpring();
+	displaySummer();
+	displayFall();
 }
 
 displayAllSeasons();
